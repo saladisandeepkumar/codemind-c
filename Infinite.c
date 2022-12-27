ea6436e468d8ac61d260527bdfd96608afba1ab7
saladisandeepkumar <119839533+saladisandeepkumar@users.noreply.github.com>
2022-12-27 18:57:30 +0530

    #include<stdio.h> int main() {     int i,a[100];     for(i=0;i<100;i++)     {         scanf("%d",&a[i]);
        }
        for(i=0;i<100;i++)
        {
        if(a[i]==-1)
        {
            break;
        }
        else
        {
            printf("%d
    ",a[i]*a[i]);
        }
    }}

---
+++ Infinite.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,a[100];
+    for(i=0;i<100;i++)
+    {
+        scanf("%d",&a[i]);
+        
+    }
+    for(i=0;i<100;i++)
+    {
+    if(a[i]==-1)
+    {
+        break;
+    }
+    else
+    {
+        printf("%d
+",a[i]*a[i]);
+    }
+}}
