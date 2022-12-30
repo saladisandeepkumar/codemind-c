01ba8ae0fb89774df70599cd1f26a9828c9b154d
saladisandeepkumar <119839533+saladisandeepkumar@users.noreply.github.com>
2022-12-30 20:00:49 +0530

    #include<stdio.h> int main() {     int n,m,k;     scanf("%d%d%d",&n,&m,&k);     if(m*k>=n)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,k;
+    scanf("%d%d%d",&n,&m,&k);
+    if(m*k>=n)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
