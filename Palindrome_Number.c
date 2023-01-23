89b7cc435497a063789b991cfebe41e6c8f433c1
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2023-01-23 21:59:53 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,rev,r,m;     for(i=0;i<n;i++)     {     scanf("%d",&a[i]);
    }
    for(i=0;i<n;i++)
    {
        rev=0;m=a[i];
        while(m>0)
        {
            r=m%10;
            rev=rev*10+r;
            m=m/10;
            
        }
        if(rev==a[i])
        {
            printf("True
    ");
        }
        else
        {
            printf("False
    ");
        }
    }}

---
+++ Palindrome_Number.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,rev,r,m;
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    
+}
+for(i=0;i<n;i++)
+{
+    rev=0;m=a[i];
+    while(m>0)
+    {
+        r=m%10;
+        rev=rev*10+r;
+        m=m/10;
+        
+    }
+    if(rev==a[i])
+    {
+        printf("True
+");
+    }
+    else
+    {
+        printf("False
+");
+    }
+}}
