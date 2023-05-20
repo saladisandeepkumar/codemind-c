05366c1c3e561bbbbca34f8f09d25017f515a969
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2023-05-20 20:34:02 +0530

    #include<stdio.h> int main() {     int a,b,i,hcf;     scanf("%d%d",&a,&b);     for(i=1;i<=a && i<=b;i++)     {         if(a%i==0 && b%i==0)         {             hcf=i;         }
        } 
        printf("%d",hcf);
    }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,a,b,h;
-    scanf("%d%d",&a,&b);
-    for(i=1;i<b;i++)
+    int a,b,i,hcf;
+    scanf("%d%d",&a,&b); 
+    for(i=1;i<=a && i<=b;i++) 
     {
-        if(a%i==0 && b%i==0) h=i;
-    }
-    printf("%d",h);
-    
+        if(a%i==0 && b%i==0)
+        {
+            hcf=i;
+        } 
+        
+    } 
+    printf("%d",hcf);
 }
